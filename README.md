0199ed5e475189050ee3fc60984138ea82c955dc
ZhiweiFan94 <zhiweifan1994@gmail.com>
2023-09-01 12:52:24 +0800

    clean_dataset

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Facebook Marketplace Recommendation Ranking System
+
+## load and clean tabular and image dataset
+### For tabular datasets: 
+Create 'clean_tabular_data.py' to clean 'Product.csv' and 'images.csv'. The program includes three blocks
+- Loading the files from AWS EC2
+- Check null values and extract categories of all products, classify each catagory with list of customed index, build the encoder and decoder
+- merge products and images files where the ids are matched, save them into 'traning_data.csv'
+
+### For image dataset:
+Create 'clean_images.py' to standardize the images on their sizes, channels for being consistent. The program contains the following functions:
+- Loading the images using 'with Image.open' 
+- Resize the image with same size of 512 pixels
+- Convert all imgages to RGB format, viz, channels=3
+- save images into the new folder
+
+
+ 
---
+++ clean_images.py
@@ -0,0 +1,42 @@
+#######Clean the image dataset#########
+# %% clean image dataset
+from PIL import Image
+import os
+
+def clean_image_data(input_folder, output_folder, final_size):
+ 
+    # List all files in the input folder
+    image_files = [f for f in os.listdir(input_folder) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
+    
+    for image_file in image_files:
+        input_path = os.path.join(input_folder, image_file)
+        
+        # Open the image
+        with Image.open(input_path) as image:
+            # Check and convert to RGB, to make consistent channels
+            if image.mode != 'RGB':
+                image = image.convert('RGB')
+            
+            # Resize the image
+            size = image.size
+            ratio = float(final_size) / max(size)
+            new_image_size = tuple([int(x * ratio) for x in size])
+            resized_image = image.resize(new_image_size, Image.ANTIALIAS)
+            
+            # Create a new image with the desired final_size
+            new_im = Image.new('RGB', (final_size, final_size))
+            new_im.paste(resized_image, ((final_size - new_image_size[0]) // 2, (final_size - new_image_size[1]) // 2))
+            
+            # Save the cleaned image to the output folder
+            output_path = os.path.join(output_folder, image_file)
+            new_im.save(output_path)
+    
+    print("Image preprocessing complete.")
+
+
+if __name__ == '__main__':
+    input_folder = 'images/'
+    output_folder = 'cleaned_images/'
+    final_size = 512  # Desired image size
+
+    clean_image_data(input_folder, output_folder, final_size)
